8d84178f2dd2c550856a23625c1d76eae7d1d613
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2025-05-02 19:42:14 +0600

    spread operator

--- module1/src/1.7.ts
+++ module1/src/1.7.ts
@@ -1,3 +1,27 @@
 {
   // spread operatior
+
+  const bros1: string[] = ["Tanmoy", "Nahid", "Rahat"];
+  const bros2: string[] = ["Mir", "Firoz", "Mizan"];
+
+  bros1.push(...bros2);
 }
+
+const mentors = {
+  typescript: "Mezba",
+  redux: "Mir",
+  dbms: "Mizan",
+};
+
+const mentors2 = {
+  prisma: "Firoz",
+  next: "Tanmoy",
+  cloud: "Nahid",
+};
+
+const mentorList = {
+  ...mentors,
+  ...mentors2,
+};
+
+console.log(mentorList);
