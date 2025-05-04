c9e89f09efdaa9e49be69407d98ad5cec07232fa
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2025-05-05 03:17:28 +0600

    unknown nulled types

---
+++ module1/src/1.12.ts
@@ -0,0 +1,40 @@
+{
+  // nullable types
+
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("Searching");
+    } else {
+      console.log("There is nothing to search");
+    }
+  };
+
+  searchName(null);
+
+  //   unknown type .. typeof
+
+  const getSpeedInmeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = (value * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else if (typeof value === "string") {
+      const [result, unit] = value.split(" ");
+      const convertedSpeed = (parseFloat(result) * 1000) / 3600;
+      console.log(`The speed is ${convertedSpeed} ms^-1`);
+    } else {
+      console.log("Wrong input");
+    }
+  };
+
+  getSpeedInmeterPerSecond(200);
+
+  //never
+
+  const throwError = (msg: string): never => {
+    throw new Error(msg);
+  };
+
+  throwError("Muskil se error hogaya");
+
+  //
+}
