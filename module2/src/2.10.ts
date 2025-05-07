490dbc03b7e3475af088e30448524c3e624f52a2
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2025-05-07 07:11:01 +0600

    mapped types and lookup

---
+++ module2/src/2.10.ts
@@ -0,0 +1,37 @@
+{
+  // Mapped types
+
+  const arrofNumbers: number[] = [1, 4, 5];
+
+  const arrofStrings: string[] = ["1", "2", "3"];
+
+  const arrofStrings2: string[] = arrofNumbers.map((number) => {
+    return number.toString();
+  });
+
+  console.log(arrofStrings2);
+
+  type AreaNumber = {
+    height: number;
+    width: number;
+  };
+
+  //   type AreaString = {
+  //     height: string;
+  //     width: string;
+  //   };
+
+  type AreaString<T> = {
+    [key in keyof T]: T[key];
+  };
+
+  const area1: AreaString<{
+    height: string;
+    width: number;
+  }> = {
+    height: "120",
+    width: 200,
+  };
+
+  //
+}
